e9ec07a5022814731c9decb1e0b7bbb7c9cc96cb
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-02-10 16:35:08 -0600

    finished String Exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,24 +1,34 @@
+import java.util.Scanner;
+
 public class StringExercise {
     public static void main(String[] args) {
 
         //String Basics.
 
         //TODO: Create a class named StringExercise with a main method.
+//        done
 
         //TODO: For each of the following output examples: create a String variable that contains the desired output and print it out to the console, you can do this with only one String variable and one print statement for each output example.
 
+//      We don't need no education
+        String a = "We don't need no education";
+        System.out.println(a);
 
-//        We don't need no education
-//        We don't need no thought control
+//      We don't need no thought control
+        String b = "We don't need no thought control";
+        System.out.println(b);
 
-//        Check "this" out!, "s inside of "s!
-
-//        In windows, the main drive is usually C:\
-
-//        I can do backslashes \, double backslashes \\,
-//        and the amazing triple backslash \\\!
-
-
+//      Check "this" out!, "s inside of "s!
+        String c = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(c);
+//
+//      In windows, the main drive is usually C:\
+        String d = "In windows, the main drive is usually C:\\";
+        System.out.println(d);
+//
+//      I can do backslashes \, double backslashes \\,
+        String e = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(e);
 
     }
 }
