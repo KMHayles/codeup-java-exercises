50bfc36c292c87a6bf786cf58a26fc92d2904096
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-02-09 23:51:17 -0600

    add notes

---
+++ src/HelloWorld.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main (String[] args){
+//        System.out.println("Hello again");
+//        int myFavoriteNumber = 7;
+////        String myString = "3.14159";
+////        float myNumber = 3.14F;
+////
+////
+////        System.out.println(myFavoriteNumber);
+////        System.out.println(myString);
+////        System.out.println(myNumber);
+
+//            int x = 5;
+//            System.out.println(x++);
+//            System.out.println(x);
+
+
+//            int x = 5;
+//            System.out.println(++x);
+//            System.out.println(x);
+        Scanner sc = new Scanner(System.in);
+        double pi = 3.14159;
+        newPi = ;
+        System.out.println("The value of pi is approximately " + pi);
+
+
+
+
+    }
+
+}
