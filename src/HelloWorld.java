36f01adb7c1e9efed533cca28967fc7b5de9490e
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2023-02-15 01:16:04 -0600

    pushed exercises for java 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -23,50 +23,6 @@ import java.lang.Math;
 //            System.out.println(++x);
 //            System.out.println(x);
 
-//
-
-//        System.out.printf("The value of pi is approximately " + pi);
-
-        //.2 is for two decimal points
-//        System.out.printf("The value of pi is approximately %.2f.", pi);
-
-//    Scanner sc = new Scanner(System.in);
-//        System.out.println("enter integer");
-//        int userInt = sc.nextInt();
-//        System.out.println(userInt);
-
-
-
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("enter 3 words");
-
-//        String word1 = sc.nextLine();
-//        String word2 = sc.nextLine();
-//        String word3 = sc.nextLine();
-
-//        String word1 = sc.next();
-//        String word2 = sc.next();
-//        String word3 = sc.next();
-
-//        System.out.print(word1 + "\n" + word2 + "\n" + word3);
-            //same
-//        System.out.printf("%s\n%s\n%s", word1, word2, word3);
-
-
-//justin's code
-//        Scanner sc = new Scanner(System.in);
-//
-//        System.out.println("Enter the length of the classroom: ");
-//        double length = Double.parseDouble(sc.nextLine());
-//
-//        System.out.println("Enter the width of the classroom: ");
-//        double width = Double.parseDouble(sc.nextLine());
-//
-//        double permieter = (length + width)*2;
-//        double area = length * width;
-//
-//        System.out.println("The parimeter is: " + permieter);
-//        System.out.println("The area is: " + area );
 
 //    }
 //}
